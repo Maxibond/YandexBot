2a6ee5f26a8c2e83309ce77fa1f0eb574db9e76d
Maxibond <jeymontor@yandex.ru>
2016-03-12 21:34:18 +0300

    drawing refactoring COLORS

--- draw.py
+++ draw.py
@@ -124,7 +124,6 @@ def drawCircle(time, tags):
     r = 100
     colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#f1c40f', '#e67e22', '#e74c3c', '#95a5a6',
               '#16a085', '#27ae60', '#2980b9', '#8e44ad', '#2c3e50', '#f39c12', '#d35400', '#c0392b', '#7f8c8d']
-    random.shuffle(colors)
     x_pos = img_w / 2
     y_pos = x_pos
     draw.ellipse((x_pos, y_pos, y_pos, x_pos), outline='black')
